3ac4f972cc34038bd7ec225f7bc8c4c7be5cda6a
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2020-11-23 21:21:01 +0300

    Update LinearRegression.py

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -1,32 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class LinearRegression:
+    def __init__(self):
+        self.__weight = np.zeros(0)
+        self.__complexity = 0
+    
+    def get_parametrs(self):
+        return {'Weight': self.__weight, 'Complexity': self.__complexity}
+    
+    def __DesignMatrix(self, x_set, M):
+        Plan_Matrix = np.zeros((x_set.shape[0], M))
+        for i in range(M):
+            Plan_Matrix[:, i] = x_set ** i
+        return Plan_Matrix
+
+    def fit(self, x_set, t_set, complexity):
+        Plan_Matrix = self.__DesignMatrix(x_set, complexity)
+        self.__complexity = complexity
+        self.__weight = np.linalg.inv(Plan_Matrix.T @ Plan_Matrix) @ Plan_Matrix.T @ t_set
+    
+    def predict(self, x_set):
+        Plan_Matrix = self.__DesignMatrix(x_set, self.__complexity)
+        return Plan_Matrix @ self.__weight
+    
+    def score(self, x_set, t_set):
+        y = self.__DesignMatrix(x_set, self.__complexity) @ self.__weight
+        return np.sum((t_set - y) ** 2) / 2
+
+
 x = np.load('dataset.npy')
 t = np.load('target.npy')
 z = np.load('realmodel.npy')
 
 N = x.shape[0]
 
-# Build Design matrix
-def DesignMatrix(M):
-    Plan_Matrix = np.zeros((N, M))
-    for i in range(M):
-        Plan_Matrix[:, i] = x ** i
-    return Plan_Matrix
-# Train w
-def GetW(Plan_Matrix):
-    Mura_Penrose = np.linalg.inv(Plan_Matrix.T @ Plan_Matrix) @ Plan_Matrix.T
-    w = Mura_Penrose @ t
-    return w
-
 # Regression model
 print('Write degree of the polynomial: ', end='')
 Step = int(input())
 
-Plan_Matrix = GetMatrix_Plan(Step)
-w = GetW(Plan_Matrix)
-y = Plan_Matrix @ w
-E = np.sum((t - y) ** 2) / 2
+model = LinearRegression()
+model.fit(x, t, Step)
+y = model.predict(x)
+E = model.score(x, t)
+
 print('Error = ' + str(E))
 plt.figure('M = ' + str(Step) + ', Error = ' + str(E))
 plt.plot(x, z, 'r-', label='Real model')
